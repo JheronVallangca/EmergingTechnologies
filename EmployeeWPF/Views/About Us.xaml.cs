ba1ae604f5133bf992edfb40fd9359b12adf6d65
Roxan Lagto <roxanlagto001@gmail.com>
2023-12-17 09:15:15 +0800

    Final Project

---
+++ EmployeeWPF/Views/About Us.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmployeeWPF
+{
+    /// <summary>
+    /// Interaction logic for About_Us.xaml
+    /// </summary>
+    public partial class About_Us : Window
+    {
+        public About_Us()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EmployeeWPF/Views/Homepage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using EmployeeWPF.Views;
+using MaterialDesignThemes.Wpf;
+
+namespace EmployeeWPF
+{
+    /// <summary>
+    /// Interaction logic for Homepage.xaml
+    /// </summary>
+    public partial class Homepage : Window
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Homepage Hp = new Homepage();
+            Hp.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow Mw = new MainWindow();
+            Mw.Show();
+        }
+    }
+}
